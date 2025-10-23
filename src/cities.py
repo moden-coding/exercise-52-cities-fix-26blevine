07de683cf9eefb2f5817ad3dd6bcd1f01ed7d2c4
26blevine <26blevine@gannacademy.org>
2025-10-23 11:43:55 -0400

    cities

--- src/cities.py
+++ src/cities.py
@@ -6,7 +6,15 @@ def cities():
     cities = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
     population = [643272, 279044, 231853, 223027, 201810]
     area = [715.48, 528.03, 689.59, 240.35, 3817.52]
-    pass
+
+    # make the rows named by city
+    data = {
+        "Population": population,
+        "Total area": area
+    }
+
+    df = pd.DataFrame(data, index=cities)
+    return df
 
 def main():
     print(cities())
